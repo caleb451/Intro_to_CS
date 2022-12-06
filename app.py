f6517827bc6c5829b68fae36eef5543cced033d6
caleb451 <43527078+caleb451@users.noreply.github.com>
2022-12-06 09:07:27 -0600

    edit

--- app.py
+++ app.py
@@ -8,10 +8,3 @@ app = Flask(__name__)
 @app.route('/')
 def home():
 	return render_template('index.html')
-
-# Start our application
-if __name__ == "__main__":
-	app.run(
-		debug=True,
-		# port = 5000
-	)
